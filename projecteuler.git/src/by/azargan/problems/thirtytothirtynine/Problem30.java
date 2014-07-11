3c757da89055ce6119500542611876d6c66709b1
Aliaksei_Vihuro <Aliaksei_Vihuro@EPBYMOGW0139.minsk.epam.com>
2014-07-11 19:13:50 +0300

    I solved problem 30 (with some hack)

--- projecteuler.git/src/by/azargan/problems/thirtytothirtynine/Problem30.java
+++ projecteuler.git/src/by/azargan/problems/thirtytothirtynine/Problem30.java
@@ -13,14 +13,53 @@
  * Find the sum of all the numbers that can be written as the sum of fifth
  * powers of their digits.
  *
+ * SOLVED: Answer is 443839
+ *
  */
 
 package by.azargan.problems.thirtytothirtynine;
 
 /**
- *
+ * @date   11/7/2014
  * @author Aliaksei_Vihuro
  */
 public class Problem30 {
 
+    private final static int POWER = 5;
+
+    private static boolean isEqualsToSumOfPowers(int number) {
+        String n = String.valueOf(number);
+        int digitCount = n.length();
+        int buffer = number;
+        boolean answer = false;
+        for (int i = 0; i < digitCount; i++) {
+            int digit = getDigit(number, i);
+            int pow = (int) Math.pow(digit, POWER);
+            if (pow > buffer && buffer != 0) {
+                break;
+            } else {
+                buffer -= pow;
+            }
+        }
+        if (buffer == 0) {
+            answer = true;
+        }
+        return answer;
+    }
+
+    private static int getDigit(int number, int position) {
+        String n = String.valueOf(number);
+        return Integer.parseInt(String.valueOf(n.charAt(position)));
+    }
+
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 2; i < 200000; i++) {
+            if (isEqualsToSumOfPowers(i)) {
+                sum += i;
+                System.out.println(i);
+            }
+        }
+        System.out.println("Answer is " + sum);
+    }
 }
